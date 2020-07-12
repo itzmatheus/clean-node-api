ab9918595fb3173ebf24c84d456d8cb1cfd788d2
Matheus Leite <matheusjoselfm@gmail.com>
2020-07-12 20:26:14 -0300

    test: ensure SignUpController return 500 if AddAccount throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -191,4 +191,23 @@ describe('SignUp Controller', () => {
 
     })
   })
+
+  test('Should return 500 AddAccount throws', () => {
+    const { sut, addAccountStub } = makeSut()
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        email: 'any@mail.com',
+        password: '12321321',
+        passwordConfirmation: '12321321'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
 })
